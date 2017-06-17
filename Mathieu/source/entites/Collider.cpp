89fd82cfb1f862ff0e51b6909e1365ec516f722c
wiyochi <mat.arq@sfr.fr>
2017-06-17 09:51:28 +0200

    Collider object

--- Mathieu/source/entites/Collider.cpp
+++ Mathieu/source/entites/Collider.cpp
@@ -2,11 +2,28 @@
 
 
 
-Collider::Collider()
+Collider::Collider(int x, int y, int w, int h, type t)
 {
+	m_rect = { x, y, w, h };
+	m_type = t;
 }
 
 
 Collider::~Collider()
 {
+}
+
+void Collider::update(int x, int y, int w, int h)
+{
+	m_rect = { x, y, w, h };
+}
+
+sf::IntRect Collider::box() const
+{
+	return m_rect;
+}
+
+Collider::type Collider::whatType() const
+{
+	return m_type;
 }
--- Mathieu/source/entites/Collider.h
+++ Mathieu/source/entites/Collider.h
@@ -1,11 +1,27 @@
 #pragma once
+#include <SFML\Graphics.hpp>
 
 class Collider
 {
 public:
-	Collider();
+	typedef enum
+	{
+		wall,
+		ground,
+		character,
+		weapon
+	} type;
+	Collider(int x, int y, int w, int h, type t);
 	~Collider();
+	void update(int x, int y, int w, int h);
+	sf::IntRect box() const;
+	type whatType() const;
 private:
-
+	int m_x;
+	int m_y;
+	int m_width;
+	int m_height;
+	sf::IntRect m_rect;
+	type m_type;
 };
 
